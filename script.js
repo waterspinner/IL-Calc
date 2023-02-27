10ef20b774e98d3c1ee6ca2ccfc40e001cb5e656
waterspinner <98507468+waterspinner@users.noreply.github.com>
2023-02-27 10:59:20 -0800

    IL calc formula and display update

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//constants
+const ilSubmitBtn = document.querySelector(".token-input");
+
 //IL Object
 const ilCalc = {
     displayValue: '0.00%',
@@ -21,5 +24,21 @@ const tokenInputs = document.querySelectorAll('.token');
 });
 
 //on form submit, execute IL formula
+function calculateIl(event){
+    event.preventDefault();
+    console.log(ilCalc.aTokenChange + 1);
+    //find value of pool
+    //take each asset price change to the power of pool weight %
+   let valueOfPool = ((ilCalc.aTokenChange + 1)**(ilCalc.aTokenWeight)) * ((ilCalc.bTokenChange + 1)**(ilCalc.bTokenWeight));
+   //value if held is token change * token weight
+   let valueIfHeld = ((ilCalc.aTokenChange + 1) * ilCalc.aTokenWeight) + ((ilCalc.bTokenChange + 1) * ilCalc.bTokenWeight);
 
+   console.log(valueOfPool);
+   console.log(valueIfHeld);
+   ilCalc.displayValue = ((valueOfPool / valueIfHeld) - 1) * -100
+   document.querySelector('.calculated-loss').textContent = `${parseFloat(ilCalc.displayValue).toFixed(2)}%`
+ }
+
+ilSubmitBtn.addEventListener('submit', calculateIl);
 // update displayValue
+
